d3bc1726205e0573dc3306bf855b35d944ead89a
idktigerz <idktigerzz@gmail.com>
2023-02-01 10:59:03 +0000

    Update PlayerActions.cs

--- Test/Assets/Scripts/Player/PlayerActions.cs
+++ Test/Assets/Scripts/Player/PlayerActions.cs
@@ -16,31 +16,38 @@ public class PlayerActions : MonoBehaviour
     int coinCounter = 0;
 
     string[] tags = { "Ground", "Wall" };
+    private Material coinColor;
+
     // Start is called before the first frame update
     void Start()
     {
         coinText.text = "Coins: " + coinCounter;
+        coinColor = coin.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Debug.Log(hintText.enabled);
+    { 
         RaycastHit hit;
         active = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, playerActivateDistance);
-        if (active && (!hit.collider.CompareTag("Ground") && (!hit.collider.CompareTag("Wall"))))
+        if (active && (!hit.collider.CompareTag("Ground") && (!hit.collider.CompareTag("Wall")) && (!hit.collider.CompareTag("throwableObject"))))
         {
             hintText.enabled = true;
-            if (Input.GetKeyDown(KeyCode.E) && hit.collider.CompareTag("Coin")){
-                Debug.Log("Catch");
-                coin.SetActive(false);
-                StartCoroutine(Timer());
-                coinCounter++;
-                coinText.text = "Coins: " + coinCounter;
+            if (hit.collider.CompareTag("Coin")){
+                coinColor.color = Color.yellow;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("Catch");
+                    coin.SetActive(false);
+                    StartCoroutine(Timer());
+                    coinCounter++;
+                    coinText.text = "Coins: " + coinCounter;
+                }
             }
         }
         else
         {
+            coinColor.color = Color.white;
             hintText.enabled = false;
         }
     }
