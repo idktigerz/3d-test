6f347e3782c3e6ff05d42cb0eb2f85f3fd9bb168
idktigerz <idktigerzz@gmail.com>
2023-02-01 09:59:11 +0000

    a

--- Test/Assets/Scripts/Player/PlayerActions.cs
+++ Test/Assets/Scripts/Player/PlayerActions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class PlayerActions : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class PlayerActions : MonoBehaviour
     public Text hintText;
     bool active = false;
     int coinCounter = 0;
+
+    string[] tags = { "Ground", "Wall" };
     // Start is called before the first frame update
     void Start()
     {
@@ -21,21 +24,28 @@ public class PlayerActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(hintText.enabled);
         RaycastHit hit;
         active = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, playerActivateDistance);
-        if(Input.GetKeyDown(KeyCode.E) && active)
+        if (active && (!hit.collider.CompareTag("Ground") && (!hit.collider.CompareTag("Wall"))))
         {
+            hintText.enabled = true;
             Debug.Log("Is active? " + active);
-            if (hit.collider.CompareTag("Coin")){
-                Debug.Log("Catch");
-                coin.SetActive(false);
-                StartCoroutine(Timer());
-                coinCounter++;
-                coinText.text = "Coins: " + coinCounter;
-            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (hit.collider.CompareTag("Coin"))
+                {
+                    Debug.Log("Catch");
+                    coin.SetActive(false);
+                    StartCoroutine(Timer());
+                    coinCounter++;
+                    coinText.text = "Coins: " + coinCounter;
+                }
+            } 
         }
         else
         {
+            hintText.enabled = false;
             Debug.Log("Is active? " + active);
         }
     }
