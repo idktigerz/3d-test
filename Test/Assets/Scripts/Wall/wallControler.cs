4de2331299f832cd30576960de5704fd0335e43d
AkaKoichi <bruninhomartins240@gmail.com>
2023-02-03 09:33:13 +0000

    made wall move

---
+++ Test/Assets/Scripts/Wall/wallControler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallControler : MonoBehaviour
+{
+    private bool gateState;
+    private Vector3 firstPos;
+    private Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPos = transform.position;
+        nextPos = transform.position + new Vector3(0, 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gateState)
+        {
+            Debug.Log("aaaaaaa");
+            var current = Vector3.MoveTowards(transform.position, nextPos, 10 * Time.deltaTime);
+            transform.position = current;
+
+        }
+        else
+        {
+            var current = Vector3.MoveTowards(transform.position, firstPos, 10 * Time.deltaTime);
+            transform.position = current;
+        }
+    }
+    public void MoveGate(bool state)
+    {
+        if (state)
+        {
+            gateState = true;
+            
+        }
+        else
+        {
+            gateState = false;
+        }
+    }
+}
